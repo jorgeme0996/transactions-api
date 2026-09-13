4202568ccffcd7aade47913ce62336d30ce30514
jorgeme0996 <jorgeme0996@gmail.com>
2026-09-12 23:33:17 -0600

    Fix testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -16,10 +16,16 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterEach(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body).toMatchObject({ healthy: true });
+      });
   });
 });
