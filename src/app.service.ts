75ad5c7609ebe415200ad75c8991fc1b99317c97
jorgeme0996 <jorgeme0996@gmail.com>
2026-09-17 00:14:25 -0600

    Change response from health

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,6 @@ export class AppService {
   getHealthCheck(): object {
     return {
       healthy: true,
-      imageTag: process.env.IMAGE_TAG ?? 'unknown',
-      buildDate: process.env.BUILD_DATE ?? 'unknown',
     };
   }
 }
